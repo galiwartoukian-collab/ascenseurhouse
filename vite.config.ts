f4ac5110e026a252ab4e87fe7aee5d7eb0ecf8eb
galiwartoukian-collab <galiwartoukian@gmail.com>
2026-04-06 13:57:14 -0700

    Fix merge conflict + base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-});
->>>>>>> 4a4bf93e23eb6dc18610bd33fd25157a91ed8efa
+  base: '/ascenseurhouse/',
+})
